73fbb63a65c3d0102e3a1a6d13283a9914c2e057
MarcBerneman <marc.berneman@outlook.com>
2020-11-03 18:59:16 +0100

    Same header for all pages

---
+++ js/script.js
@@ -0,0 +1,4 @@
+function createHeader() {
+ document.getElementById("header").innerHTML =
+  '<a href="./index.html">Home</a> | <a href="./about.html">CV</a> | <a href="./educational.html">Educational</a><br /><br />';
+}
